2c3e6851a903ff8a1448b52455ca3fcfd8cba3e7
Lemsito22 <lemquilca22@gmail.com>
2026-01-23 13:00:42 +0100

    .

--- Wordle/src/bkp.java
+++ Wordle/src/bkp.java
@@ -1,22 +1,37 @@
 import java.util.Random;
 import java.util.Scanner;
 
-public class main {
+public class bkp {
     public static int getrandom(){
         Random random = new Random();
         return random.nextInt(100);
 
     }
     public static String getpalabra(String[] listapalabras, int posicionpalabra){
-         return listapalabras[posicionpalabra];
+        String  palabraescogida=listapalabras[posicionpalabra];
+        return palabraescogida;
     }
     public static void print (String palabra){
         for (int i = 0; i < palabra.length(); i++) {
             System.out.print("_ ");
         }
     }
-    public static void main(String[] args) {
+    public static String verificarpalabra(String palabrauser, String palabraescogida){
         Scanner sc=new Scanner(System.in);
+        boolean comprobar=true;
+        try{
+            palabrauser=sc.next();
+            if (!(palabrauser.length()>palabraescogida.length())){
+                throw new Exception("La palabra es de maximo"+palabraescogida.length()+" letras");
+            }
+            comprobar=false;
+        }catch (Exception e){
+            System.out.println(e);
+        }
+        return palabrauser;
+    }
+    public static void main(String[] args) {
+
         String[] listapalabras= {"Agua", "Arena", "Arroz", "Asado", "Audio", "Baile", "Barco", "Bello", "Broma", "Busca",
                 "Cable", "Calor", "Canto", "Carro", "Cebra", "Chapa", "Cielo", "Cofre", "Comer", "Cuadro",
                 "Dardo", "Datos", "Debajo", "Dedo", "Deseo", "Disco", "Dicha", "Dosis", "Duda", "Dueño",
@@ -29,13 +44,15 @@ public class main {
                 "Madre", "Mago", "Mapa", "Marco", "Martes", "Marzo", "Mesa", "Metro", "Miedo", "Móvil"};
         int intentos=5, filas=5;
         int posicionpalabra= getrandom();
-        String caracterespalabra= getpalabra(listapalabras, posicionpalabra);
+        String palabrauser="";
+        String palabraescogida=getpalabra(listapalabras, posicionpalabra);
+        String caracterespalabra= palabraescogida;
         System.out.println("LA PALABRA DEL DÍA");
         print(caracterespalabra);
         System.out.println();
         System.out.println("INGRESA LA PALABRA");
-        String p=sc.next();
-        
+        palabrauser=verificarpalabra(palabrauser, palabraescogida);
+        System.out.println(palabrauser);
 
 
 
